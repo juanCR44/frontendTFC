5ae007f4dc01eeb9d38ddc8f6c54d74ad6abe01f
juanCR44 <juancastror.1235@gmail.com>
2021-11-20 16:24:06 -0500

    tf

---
+++ src/app/app.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { UserService } from './services/user.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  titulo = '';
+  post = '';
+  fileString = '';
+  buttonVisible = false;
+  msg: string = '';
+  uploadForm!: FormGroup; 
+
+  constructor(private userService: UserService, private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    /*this.userService.getMsg().subscribe(res => {
+      this.msg = JSON.stringify(res);
+    })*/
+
+    this.uploadForm = this.formBuilder.group({
+      profile: ['']
+    });
+  }
+/*
+  addPost() {
+    this.userService.addPost({ title: this.titulo, post: this.post }).subscribe(res => console.log(res))
+  }
+*/
+  changeListener($event: Event): void {
+    this.readThis($event.target)
+  }
+
+  readThis(inputValue: any) {
+    var file = inputValue.files[0];
+    this.uploadForm.get('profile')!.setValue(file);
+    this.buttonVisible = true;
+/*
+    var myReader: FileReader = new FileReader();
+    var fileType = inputValue.parentElement.id;
+    myReader.onloadend = (e) => {
+      this.fileString = myReader.result as string;
+      this.buttonVisible = true;
+   };
+
+    myReader.readAsText(file);*/
+  }
+
+  uploadFile(){
+    //this.userService.addFile(this.fileString).subscribe(r =>console.log(r));
+    const formData = new FormData();
+    formData.append('file', this.uploadForm.get('profile')!.value);
+    this.userService.addFile(formData).subscribe(r=>console.log(r));
+
+    /*this.httpClient.post<any>(this.SERVER_URL, formData).subscribe(
+      (res) => console.log(res),
+      (err) => console.log(err)
+    );*/
+  }
+
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, ObservedValueOf } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private urlBase = "http://localhost:8080";
+  private httpHeaders = new HttpHeaders({'Content-Type':'application/json'});
+  private httpHeadersFile = new HttpHeaders({
+    // 'Content-Type': undefined,
+    'Accept': '*/*',
+  })
+
+  
+
+  constructor(private http: HttpClient){}
+
+
+  getMsg():Observable<any>{
+    return this.http.get(`${this.urlBase}/ping`)
+  }
+
+  addPost(item:Object):Observable<Object>{
+    return this.http.post(this.urlBase + '/newsfeed', item, {headers:this.httpHeaders});
+  }
+
+  addFile(file:any):Observable<Object>{
+    return this.http.post(this.urlBase + '/upload', file, {headers:this.httpHeadersFile});
+  }
+/*
+  createPublicador(publicador: object): Observable<Object>{
+    console.log("Enviando rest create...")
+    return this.http.post(this.urlBase + '/registrarpublicador', publicador, {headers:this.httpHeaders});
+  }*/
+}
